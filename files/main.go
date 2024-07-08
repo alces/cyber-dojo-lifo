8da384b8eb58599283c26355a75b51ef38f7b1b5
1fMyeG <1fMyeG@cyber-dojo.org>
2024-07-08 08:43:13 +0000

    4 ran tests, predicted green, got green

--- files/main.go
+++ files/main.go
@@ -1,5 +1,9 @@
 package lifo
 
+import (
+    "fmt"
+)
+
 type Stack struct {
     size int
 }
@@ -9,5 +13,9 @@ func New() *Stack {
 }
 
 func (s *Stack) Add(element string) error {
+    if element == "" {
+        return fmt.Errorf("Element cannot be empty")
+    }
+    
     return nil
 }

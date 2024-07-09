6603d8704656fd5eac06be6493179ea0a337ff39
1fMyeG <1fMyeG@cyber-dojo.org>
2024-07-09 09:44:06 +0000

    26 ran tests, predicted green, got green

--- files/main.go
+++ files/main.go
@@ -37,5 +37,11 @@ func (s *Stack) Size() int {
 }
 
 func (s *Stack) index(element string) int {
-    return 0
+    for i := 0; i < s.Size(); i++ {
+        if s.items[i] == element {
+            return i
+        }
+    }
+    
+    return -1
 }

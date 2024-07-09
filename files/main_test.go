e784114c30c1af85bb4317580afd3f8c6b1b30a4
1fMyeG <1fMyeG@cyber-dojo.org>
2024-07-09 09:45:46 +0000

    27 ran tests, predicted green, got green

--- files/main_test.go
+++ files/main_test.go
@@ -46,8 +46,15 @@ func TestStackGet(t *testing.T) {
 }
 
 func TestStackIndex(t *testing.T) {
+    nonexistent := "nonexistent"
+    existing := "existing"
+    
     stack := New()
-    assert.Equal(t, -1, stack.index("nonexistent"))
+    assert.Equal(t, -1, stack.index(nonexistent))
+        
+    stack.Add(existing)
+    assert.Equal(t, 0, stack.index(existing))
+    assert.Equal(t, -1, stack.index(nonexistent))
 }
     
 func TestStackSize(t *testing.T) {
